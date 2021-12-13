b05165848347a0158f89b5ae4f034834aa9c301f
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-12-13 23:40:08 +0100

    MDL-73348 phpunit: normalize all privacy provider tests
    As far as now all them have correct privacy level2 namespace:
    - Move them to "privacy" subdir.
    - Rename the files to "provider_test.php", this includes old
      privacy_test.php and privacy_provider_test.php files
    - Rename the testcase to provider_test too (to match file name)
    
    Also, change some relative paths and comments to point to new
    locations.

--- tests/privacy/provider_test.php
+++ tests/privacy/provider_test.php
@@ -26,7 +26,7 @@ namespace mod_assignment\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once(__DIR__ . '/../lib.php');
+require_once(__DIR__ . '/../../lib.php');
 
 use mod_assignment\privacy\provider;
 use core_privacy\local\request\approved_contextlist;
@@ -42,7 +42,7 @@ use core_privacy\tests\provider_testcase;
  * @copyright  2018 Zig Tan <zig@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class privacy_test extends provider_testcase {
+class provider_test extends provider_testcase {
 
     /**
      * @var int array   Array of test student ids associated for Course 1.
